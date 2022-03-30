bcdf539d34a6146b3d443d7825252516522eed18
BrenoRCBrito <brenorcbrito@gmail.com>
2022-03-30 17:50:11 -0300

    conserta lint #2

--- src/utils/joiSchemas.ts
+++ src/utils/joiSchemas.ts
@@ -38,9 +38,9 @@ export const user = Joi.object({
 
 export const order = Joi.object({
   products: Joi.array().min(1).items(Joi.number()).required()
-  .messages({
-    'any.required': 'Products is required',
-    'array.base': 'Products must be an array of numbers',
-    'array.min': 'Products can\'t be empty',
-  }),
+    .messages({
+      'any.required': 'Products is required',
+      'array.base': 'Products must be an array of numbers',
+      'array.min': 'Products can\'t be empty',
+    }),
 });
